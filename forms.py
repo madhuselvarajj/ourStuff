261d98f491a1641cd77a71515a72d29ad5b2f957
Navjotsk <60015459+Navjotsk@users.noreply.github.com>
2021-03-30 20:33:00 -0600

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, DateField
 from wtforms.validators import DataRequired
 
 
@@ -7,3 +7,15 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()]) #validators that field is not null and is valid email
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+   
+class registerForm(FlaskForm):
+    lname = StringField('last name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    dob = DateField('Date', validators=[DataRequired()])
+    street = StringField('street', validators=[DataRequired()])
+    city = StringField('City', validators=[DataRequired()])
+    province = StringField('province', validators=[DataRequired()])
+    postalCode = StringField('postal code', validators=[DataRequired()])
+    fname = StringField('first name', validators=[DataRequired()])
+    submit = SubmitField('Register')
