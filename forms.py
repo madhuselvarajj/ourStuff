4571b0f2a945239375748a9d89770d628eec12f5
Navjotsk <60015459+Navjotsk@users.noreply.github.com>
2021-04-06 16:46:21 -0600

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, DateField
+from wtforms import StringField, PasswordField, SubmitField, DateField, IntegerField
 from wtforms.validators import DataRequired
 from wtforms.widgets import PasswordInput
 
