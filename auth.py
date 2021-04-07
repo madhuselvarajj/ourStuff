09025707a24402705400fb648cfd65561da5ea53
Stephane Dorotich <stephanedorotich@gmail.com>
2021-04-07 17:24:50 -0600

    Browse item table format updated. Sign-up link added to login page.

--- auth.py
+++ auth.py
@@ -15,11 +15,13 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.before_app_request
 def load_logged_in_user():
+    # session['user_id'] is set by Login() and passed on before every
+    # app request until the user logs out.
     user_id = session.get('user_id')
 
     if user_id is None:
         g.user = None
-    else:
+    else:  
         g.user = get_db().execute(
             'SELECT * FROM user WHERE email = ?', (user_id,)
         ).fetchone()
